f5af8c401e04e135dd1cd9a99397c8aff7bc9a57
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-11-10 18:47:07 -0800

    created a method that checks if the entered number is an armstrong number of not, for a number to be armstrong take cube of every single digit in it and then add them, if the resultant is same as the input then the number is an armstrong number.

--- src/FindArmstrongNumber.java
+++ src/FindArmstrongNumber.java
@@ -1,15 +1,32 @@
 import java.util.Scanner;
 
 public class FindArmstrongNumber {
+    static boolean checkEqual(int x, int y){
+        if(x==y){
+            return true;
+        }
+        return false;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter the number to be checked.");
         int input = sc.nextInt();
         int x = input;
 
+        int sum = 0;
         while(input !=0){
             int r = input%10;
-            int n = input/10;
+            input = input/10;
+
+            sum +=(int) Math.pow(r,3);
+
         }
+        System.out.println("Result of the number tested is: " + checkEqual(x,sum));
+
+        System.out.printf("Output from the entered number is: " + sum);
+
+
+
+
     }
 }
