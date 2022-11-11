faaf6b227b1e2b34760b026467aed2df0377f3ee
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-11-10 18:28:52 -0800

    Initial commit

---
+++ src/FindArmstrongNumber.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class FindArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the number to be checked.");
+        int input = sc.nextInt();
+        int x = input;
+
+        while(input !=0){
+            int r = input%10;
+            int n = input/10;
+        }
+    }
+}
